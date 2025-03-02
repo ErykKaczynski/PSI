381ec63c1842ff803f6b4048ff008b8926b5e711
ErykKaczynski <e.kaczynski2@student.uw.edu.pl>
2025-03-02 21:40:16 +0100

    Add files via upload

---
+++ zaj_1_zad.R
@@ -0,0 +1,10 @@
+
+# 3. Stwórz funkcję o nazwie pole_kola, która oblicza pole powierzchni koła dla danego promienia.
+pole_kola = function(r){
+  return(pi*r^2)
+}
+
+# 4. Stwórz funkcję, która oblicza długość przeciwprostokątnej w trójkącie prostokątnym.
+przeciwprostokatna = function(a,b){
+  return(sqrt(a^2 + b^2))
+}
